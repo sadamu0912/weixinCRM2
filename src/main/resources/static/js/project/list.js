970410526fe11e4bfc3de9ba960cf062b201a482
xujunxia <1147644413@qq.com>
2018-01-23 21:52:09 +0800

    添加magic-checkbox

--- src/main/resources/static/js/project/list.js
+++ src/main/resources/static/js/project/list.js
@@ -7,6 +7,7 @@
 			},
 			init:function(){
 				this.picHoverChangeOpacity();
+				this.changeSort();
 			},
 			/**
 			 * 当鼠标停在图片上的时候，改变透明度
@@ -17,6 +18,21 @@
 			})
 			$('#top10project').on('mouseleave','img',function(event){
 				$(this).removeClass('hoverd');
+			})
+		},
+		/**
+		 * 点击图标改变排序的顺序
+		 */
+		changeSort:function(){
+			$('.main-content').on('click','.canOrderTHead',function(event){
+				var icon = $(this).find('i');
+				if(icon.hasClass('fa-sort-up')){
+					icon.removeClass('fa-sort-up').addClass('fa-sort-desc');
+					
+				}else if(icon.hasClass('fa-sort-desc')){
+					icon.removeClass('fa-sort-desc').addClass('fa-sort-up');	
+				}
+				
 			})
 		}
 	}
